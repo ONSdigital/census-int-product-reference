f991f359cbf298390c63d8962601e287a323560b
Phil Whiles <phil.whiles@gmail.com>
2019-04-19 22:24:18 +0100

    deps updated and builder added to Product

--- src/main/java/uk/gov/ons/ctp/integration/common/product/model/Product.java
+++ src/main/java/uk/gov/ons/ctp/integration/common/product/model/Product.java
@@ -1,9 +1,15 @@
 package uk.gov.ons.ctp.integration.common.product.model;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
   private String fulfilmentCode;
   private String description;
